70c8978f01aa3b8a49cfa4b7d9d48a528784e9ef
Ujjwal Gupta <ujjwal.gupta@aula.ac.uk>
2025-07-02 19:41:41 +0530

    github deploy attempt 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/image-qr-code/',
   build: {
     outDir: "docs", // Output build to /docs
-    assetsDir: ".", // Place assets at the root of /docs
   },
 });
