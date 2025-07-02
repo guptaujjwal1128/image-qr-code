9f51221e9259b643c817d8c1f2c5b1d5931879bd
Ujjwal Gupta <ujjwal.gupta@aula.ac.uk>
2025-07-02 17:36:02 +0530

    implemnted front end mentor part

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello World !</h1>
-    </>
+    <main className="container">
+      <div className="card">
+        <img
+          className="card-img"
+          src="src/assets/images/image-qr-code.png"
+          alt="QR Code"
+        />
+        <div className="card-content">
+          <div className="card-content-primary">
+            Improve your front-end skills by building projects
+          </div>
+          <div className="card-content-secondary">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
